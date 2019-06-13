203184e9eb4bc50caec0f01a5c816266a4d0a7e0
Vadim Hatsura <vadimgatsura@gmail.com>
2019-06-13 17:26:48 +0300

    Enhance project file reader

--- tests/Analyzers/Sourcelyzer.Analyzing.NuGet.Tests/Reader/ProjectFileReaderTests.cs
+++ tests/Analyzers/Sourcelyzer.Analyzing.NuGet.Tests/Reader/ProjectFileReaderTests.cs
@@ -48,6 +48,74 @@ namespace Sourcelyzer.Analyzing.NuGet.Tests.Reader
                         }
                     }
                 };
+
+                yield return new object[]
+                {
+                    XDocument.Parse(@"
+<Project Sdk=""Microsoft.NET.Sdk"">
+    <PropertyGroup>
+        <TargetFramework>netstandard2.0</TargetFramework>
+    </PropertyGroup>
+    <ItemGroup>
+        <ProjectReference Include=""..\\Sourcelyzer\\Sourcelyzer.csproj"" />
+    </ItemGroup>
+    <ItemGroup>
+        <PackageReference Include=""morelinq"" >
+            <Version>3.1.1</Version>
+        </PackageReference>
+        <PackageReference Include=""Octokit"">
+            <Version>0.32.0</Version>
+        </PackageReference>
+    </ItemGroup>
+</Project>"),
+                    new Action<PackageReference>[]
+                    {
+                        package =>
+                        {
+                            Assert.Equal("morelinq", package.PackageIdentity.Id);
+                            Assert.Equal(new NuGetVersion(3, 1, 1), package.PackageIdentity.Version);
+                        },
+                        package =>
+                        {
+                            Assert.Equal("Octokit", package.PackageIdentity.Id);
+                            Assert.Equal(new NuGetVersion(0, 32, 0), package.PackageIdentity.Version);
+                        }
+                    }
+                };
+                
+                yield return new object[]
+                {
+                    XDocument.Parse(@"
+<Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+    <PropertyGroup>
+        <TargetFramework>netstandard2.0</TargetFramework>
+    </PropertyGroup>
+    <ItemGroup>
+        <ProjectReference Include=""..\\Sourcelyzer\\Sourcelyzer.csproj"" />
+    </ItemGroup>
+    <ItemGroup>
+        <PackageReference Include=""morelinq"" >
+            <Version>3.1.1</Version>
+        </PackageReference>
+        <PackageReference Include=""Octokit"">
+            <Version>0.32.0</Version>
+        </PackageReference>
+    </ItemGroup>
+</Project>"),
+                    new Action<PackageReference>[]
+                    {
+                        package =>
+                        {
+                            Assert.Equal("morelinq", package.PackageIdentity.Id);
+                            Assert.Equal(new NuGetVersion(3, 1, 1), package.PackageIdentity.Version);
+                        },
+                        package =>
+                        {
+                            Assert.Equal("Octokit", package.PackageIdentity.Id);
+                            Assert.Equal(new NuGetVersion(0, 32, 0), package.PackageIdentity.Version);
+                        }
+                    }
+                };
             }
         }
 
