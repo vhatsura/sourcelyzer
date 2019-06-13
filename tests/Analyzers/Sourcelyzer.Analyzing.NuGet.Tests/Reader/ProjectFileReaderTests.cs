41dfc3b8d72ffc27de6e4a52f8f9a5d3436d6e8a
Vadim Hatsura <vadimgatsura@gmail.com>
2019-06-13 19:14:10 +0300

    remove duplicated code

--- tests/Analyzers/Sourcelyzer.Analyzing.NuGet.Tests/Reader/ProjectFileReaderTests.cs
+++ tests/Analyzers/Sourcelyzer.Analyzing.NuGet.Tests/Reader/ProjectFileReaderTests.cs
@@ -19,6 +19,20 @@ namespace Sourcelyzer.Analyzing.NuGet.Tests.Reader
                     new XDocument()
                 };
 
+                var elementInspectors = new Action<PackageReference>[]
+                {
+                    package =>
+                    {
+                        Assert.Equal("morelinq", package.PackageIdentity.Id);
+                        Assert.Equal(new NuGetVersion(3, 1, 1), package.PackageIdentity.Version);
+                    },
+                    package =>
+                    {
+                        Assert.Equal("Octokit", package.PackageIdentity.Id);
+                        Assert.Equal(new NuGetVersion(0, 32, 0), package.PackageIdentity.Version);
+                    }
+                };
+                
                 yield return new object[]
                 {
                     XDocument.Parse(@"
@@ -34,19 +48,7 @@ namespace Sourcelyzer.Analyzing.NuGet.Tests.Reader
         <PackageReference Include=""Octokit"" Version=""0.32.0"" />
     </ItemGroup>
 </Project>"),
-                    new Action<PackageReference>[]
-                    {
-                        package =>
-                        {
-                            Assert.Equal("morelinq", package.PackageIdentity.Id);
-                            Assert.Equal(new NuGetVersion(3, 1, 1), package.PackageIdentity.Version);
-                        },
-                        package =>
-                        {
-                            Assert.Equal("Octokit", package.PackageIdentity.Id);
-                            Assert.Equal(new NuGetVersion(0, 32, 0), package.PackageIdentity.Version);
-                        }
-                    }
+                    elementInspectors    
                 };
 
                 yield return new object[]
@@ -68,19 +70,7 @@ namespace Sourcelyzer.Analyzing.NuGet.Tests.Reader
         </PackageReference>
     </ItemGroup>
 </Project>"),
-                    new Action<PackageReference>[]
-                    {
-                        package =>
-                        {
-                            Assert.Equal("morelinq", package.PackageIdentity.Id);
-                            Assert.Equal(new NuGetVersion(3, 1, 1), package.PackageIdentity.Version);
-                        },
-                        package =>
-                        {
-                            Assert.Equal("Octokit", package.PackageIdentity.Id);
-                            Assert.Equal(new NuGetVersion(0, 32, 0), package.PackageIdentity.Version);
-                        }
-                    }
+                    elementInspectors
                 };
                 
                 yield return new object[]
@@ -102,19 +92,7 @@ namespace Sourcelyzer.Analyzing.NuGet.Tests.Reader
         </PackageReference>
     </ItemGroup>
 </Project>"),
-                    new Action<PackageReference>[]
-                    {
-                        package =>
-                        {
-                            Assert.Equal("morelinq", package.PackageIdentity.Id);
-                            Assert.Equal(new NuGetVersion(3, 1, 1), package.PackageIdentity.Version);
-                        },
-                        package =>
-                        {
-                            Assert.Equal("Octokit", package.PackageIdentity.Id);
-                            Assert.Equal(new NuGetVersion(0, 32, 0), package.PackageIdentity.Version);
-                        }
-                    }
+                    elementInspectors
                 };
             }
         }
